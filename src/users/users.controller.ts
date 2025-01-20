f29a4bfe8f93ad90ee547b05a5778978bfceda6b
Erdem Guldur <erdem@erdemguldur.com>
2025-01-20 10:30:52 +0300

    routing

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-  @Get('getUsers')
-  getUsers() {
+  @Get()
+  findAll() {
     return this.usersService.getUsers();
+  }
+
+  @Get(':index')
+  findOne(@Param('index') index: string) {
+    return this.usersService.getUser(index);
+  }
+
+  @Post()
+  create(@Body() username: string) {
+    return this.usersService.createUser(username);
+  }
+
+  @Put(':index')
+  update(@Param('index') index: string, @Body() username: string) {
+    return this.usersService.updateUser(index, username);
+  }
+
+  @Delete(':index')
+  delete(@Param('index') index: number) {
+    return this.usersService.deleteUser(index);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,32 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
+  private users: string[] = [];
+
+  constructor() {
+    this.users = ['ahmet', 'mehmet', 'ali', 'veli'];
+  }
+
   getUsers() {
-    return 'Hello World services';
+    return this.users;
+  }
+
+  getUser(index: string) {
+    return this.users[index];
+  }
+
+  createUser(username: string) {
+    this.users.push(username);
+    return 'Kullanıcı oluşturuldu';
+  }
+
+  updateUser(index: string, username: string) {
+    this.users[index] = username;
+    return 'Kullanıcı güncellendi';
+  }
+
+  deleteUser(index: number) {
+    this.users.splice(index, 1);
+    return 'Kullanıcı silindi';
   }
 }
