d75d6ee9de37117ce1060db8c106c3c96fbc9cf0
Erdem Guldur <erdem@erdemguldur.com>
2025-04-25 14:58:31 +0300

    Enhance application configuration and database connection setup
    - Added @nestjs/config and @nestjs/mongoose dependencies to package.json and package-lock.json.
    - Configured global ConfigModule and established a MongoDB connection using MongooseModule in app.module.ts, including connection event logging.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,36 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
+// 0: 'disconnected',
+// 1: 'connected',
+// 2: 'connecting',
+// 3: 'disconnecting',
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+        connectionFactory: (connection) => {
+          if (connection.readyState === 1) {
+            console.log('MongoDB connected');
+          }
+          connection.on('connected', () => {
+            console.log('MongoDB connected event');
+          });
+          connection.on('error', (error) => {
+            console.log('MongoDB connection error', error);
+          });
+          return connection;
+        },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
